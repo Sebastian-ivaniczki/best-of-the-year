027103ec20435e5935c79b56cab6844ce5095ae2
Sebastian-ivaniczki <sivaniczki@gmail.com>
2023-05-24 16:45:13 +0200

    hotfix

--- src/main/java/group/java/demo/controller/Mycontroller.java
+++ src/main/java/group/java/demo/controller/Mycontroller.java
@@ -6,10 +6,8 @@ import java.util.List;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
+
 
 import group.java.demo.enity.Movie;
 import group.java.demo.enity.Song;
